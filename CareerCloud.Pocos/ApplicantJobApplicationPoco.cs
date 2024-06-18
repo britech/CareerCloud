01c796086ec73ae3b637e23d2840ec4bf2595092
Brian Angyang <n01713422@humber.ca>
2024-06-18 03:52:58 -0400

    add mapping: Applicant_Job_Applications

---
+++ CareerCloud.Pocos/ApplicantJobApplicationPoco.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Applicant_Job_Applications")]
+    public class ApplicantJobApplicationPoco : IPoco
+    {
+        [Key]
+        [Column(name: "Id")]
+        public Guid Id { get; set; }
+
+        [Column(name: "Applicant")]
+        public Guid Applicant { get; set; }
+
+        [Column(name: "Job")]
+        public Guid Job { get; set; }
+
+        [Column(name: "Application_Date")]
+        public DateTime ApplicationDate { get; set; }
+
+        [Column(name: "Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+    }
+}
