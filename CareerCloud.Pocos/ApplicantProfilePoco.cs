e79929b53b4977c95478d3e8c0a623c437582576
Brian Angyang <n01713422@humber.ca>
2024-06-18 04:13:23 -0400

    align with unit test

--- CareerCloud.Pocos/ApplicantProfilePoco.cs
+++ CareerCloud.Pocos/ApplicantProfilePoco.cs
@@ -7,10 +7,8 @@ namespace CareerCloud.Pocos
     public class ApplicantProfilePoco : IPoco
     {
         [Key]
-        [Column(name: "Id")]
         public Guid Id { get; set; }
 
-        [Column(name: "Login")]
         public Guid Login { get; set; }
 
         [Column(name: "Current_Salary")]
@@ -23,19 +21,19 @@ namespace CareerCloud.Pocos
         public string? Currency { get; set; }
 
         [Column(name: "Country_Code")]
-        public string? CountryCode { get; set; }
+        public string? Country { get; set; }
 
         [Column(name: "State_Province_Code")]
-        public string? StateProvinceCode { get; set; }
+        public string? Province { get; set; }
 
         [Column(name: "Street_Address")]
-        public string? StreetAddress { get; set; }
+        public string? Street { get; set; }
 
         [Column(name: "City_Town")]
-        public string? CityTown {  get; set; }
+        public string? City {  get; set; }
 
         [Column(name: "Zip_Postal_Code")]
-        public string? ZipPostalCode {  get; set; }
+        public string? PostalCode {  get; set; }
 
         [Column(name: "Time_Stamp")]
         public byte[] TimeStamp { get; set; }
