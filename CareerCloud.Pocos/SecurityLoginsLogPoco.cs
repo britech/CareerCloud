46804ac4ae5fa324bca7ff875d19e5334343b631
Brian Angyang <n01713422@humber.ca>
2024-06-17 22:13:01 -0400

    add mapping: Security_Logins_Log

---
+++ CareerCloud.Pocos/SecurityLoginsLogPoco.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Security_Logins_Log")]
+    public class SecurityLoginsLogPoco : IPoco
+    {
+        [Key]
+        [Column(name: "Id")]
+        public Guid Id { get; set; }
+
+        [Column(name: "Login")]
+        public Guid Login {  get; set; }
+
+        [Column(name: "Source_IP")]
+        public string SourceIP { get; set; }
+
+        [Column(name: "Logon_Date")]
+        public DateTime LogonDate { get; set; }
+
+        [Column(name: "Is_Succesful")]
+        public bool IsSuccesful {  get; set; }
+    }
+}
