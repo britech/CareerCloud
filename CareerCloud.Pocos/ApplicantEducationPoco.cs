63ff5ad5811542025484da0ed9ea34723a47ff97
Brian Angyang <n01713422@humber.ca>
2024-06-18 03:46:34 -0400

    add mapping: Applicant_Educations

---
+++ CareerCloud.Pocos/ApplicantEducationPoco.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Applicant_Educations")]
+    public class ApplicantEducationPoco : IPoco
+    {
+        [Key]
+        [Column(name: "Id")]
+        public Guid Id { get; set; }
+
+        [Column(name: "Applicant")]
+        public Guid Applicant { get; set; }
+
+        [Column(name: "Major")]
+        public string Major {  get; set; }
+
+        [Column(name: "Certificate_Diploma")]
+        public string? CertificateDiploma { get; set; }
+
+        [Column(name: "Start_Date")]
+        public DateTime? StartDate { get; set; }
+
+        [Column(name: "Completion_Date")]
+        public DateTime? CompletionDate { get; set; }
+
+        [Column(name: "Completion_Percent")]
+        public Byte? CompletionPercent { get; set; }
+
+        [Column(name: "Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+    }
+}
