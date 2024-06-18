77271ef31f6fbaebdeac832fc6da9c8e5f9d48b7
Brian Angyang <n01713422@humber.ca>
2024-06-18 03:57:27 -0400

    add mapping: Applicant_Resumes

---
+++ CareerCloud.Pocos/ApplicantResumePoco.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Applicant_Resumes")]
+    public class ApplicantResumePoco : IPoco
+    {
+        [Key]
+        [Column(name: "Id")]
+        public Guid Id { get; set; }
+
+        [Column(name: "Applicant")]
+        public Guid Applicant { get; set; }
+
+        [Column(name: "Resume")]
+        public string Resume { get; set; }
+
+        [Column(name: "Last_Updated")]
+        public DateTime? LastUpdated { get; set; }
+    }
+}
