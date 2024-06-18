0cefdea61722b7d62360e8466118e12900defd50
Brian Angyang <n01713422@humber.ca>
2024-06-18 04:27:40 -0400

    add mapping: Applicant_Work_History

---
+++ CareerCloud.Pocos/ApplicantWorkHistoryPoco.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Applicant_Work_History")]
+    public class ApplicantWorkHistoryPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Applicant { get; set; }
+
+        [Column(name: "Company_Name")]
+        public string CompanyName { get; set; }
+
+        [Column(name: "Country_Code")]
+        public string CountryCode { get; set; }
+
+        [Column(name: "Location")]
+        public string Location { get; set; }
+
+        [Column(name: "Job_Title")]
+        public string JobTitle { get; set; }
+
+        [Column(name: "Job_Description")]
+        public string JobDescription { get; set; }
+
+        [Column(name: "Start_Month")]
+        public Int16 StartMonth { get; set; }
+
+        [Column(name: "Start_Year")]
+        public int StartYear { get; set; }
+
+        [Column(name: "End_Month")]
+        public Int16 EndMonth { get; set; }
+
+        [Column(name: "End_Year")]
+        public int EndYear { get; set; }
+
+        [Column(name: "Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+    }
+}
