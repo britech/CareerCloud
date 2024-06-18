8eb4edf59b8fd8e901def17b5776f52430953f48
Brian Angyang <n01713422@humber.ca>
2024-06-17 22:19:41 -0400

    add mapping: Security_Logins_Roles

---
+++ CareerCloud.Pocos/SecurityLoginsRolePoco.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Security_Logins_Roles")]
+    public class SecurityLoginsRolePoco : IPoco
+    {
+        [Key]
+        [Column(name: "Id")]
+        public Guid Id { get; set; }
+
+        [Column(name: "Login")]
+        public Guid Login {  get; set; }
+
+        [Column(name: "Role")]
+        public Guid Role { get; set; }
+
+        [Column(name: "Time_Stamp")]
+        public byte[]? TimeStamp { get; set; }
+    }
+}
