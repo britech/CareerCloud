b552570d7419487edcb61903469f2d724f233ec6
Brian Angyang <n01713422@humber.ca>
2024-06-18 04:06:55 -0400

    add mapping: Applicant_Skills

---
+++ CareerCloud.Pocos/ApplicantSkillPoco.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Applicant_Skills")]
+    public class ApplicantSkillPoco : IPoco
+    {
+        [Key]
+        [Column(name: "Id")]
+        public Guid Id { get; set; }
+
+        [Column(name: "Applicant")]
+        public Guid Applicant { get; set; }
+
+        [Column(name: "Skill")]
+        public string Skill { get; set; }
+
+        [Column(name: "Skill_Level")]
+        public string SkillLevel { get; set; }
+
+        [Column(name: "Start_Month")]
+        public Byte StartMonth { get; set; }
+
+        [Column(name: "Start_Year")]
+        public int StartYear { get; set; }
+
+        [Column(name: "End_Month")]
+        public Byte EndMonth { get; set; }
+
+        [Column(name: "End_Year")]
+        public Byte EndYear { get; set; }
+
+        [Column(name: "Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+    }
+}
