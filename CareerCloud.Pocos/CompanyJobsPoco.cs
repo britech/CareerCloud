1c4f77146c3543b461d2de6e1d790afee284749a
Brian Angyang <n01713422@humber.ca>
2024-06-18 04:40:29 -0400

    add mapping: Company_Jobs

---
+++ CareerCloud.Pocos/CompanyJobsPoco.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table(name: "Company_Jobs")]
+    public class CompanyJobsPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Company {  get; set; }
+
+        [Column(name: "Profile_Created")]
+        public DateTime ProfileCreated { get; set; }
+
+        [Column(name: "Is_Inactive")]
+        public bool IsInactive { get; set; }
+
+        [Column(name: "Is_Company_Hidden")]
+        public bool IsCompanyHidden {  get; set; }
+
+        [Column(name: "Time_Stamp")]
+        public byte[]? TimeStamp { get; set; }
+    }
+}
