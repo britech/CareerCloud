fd0c8275ddb59ed1060ba4ca30b680fdac7ea4d0
Brian Angyang <n01713422@humber.ca>
2024-07-09 20:30:06 -0400

    add partial implementation of base repository to centralize connection string

---
+++ CareerCloud.ADODataAccessLayer/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using CareerCloud.DataAccessLayer;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public abstract class BaseRepository<T> : IDataRepository<T>
+    {
+        protected const string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=JOB_PORTAL_DB;Integrated Security=true";
+
+        public abstract void Add(params T[] items);
+        public abstract void CallStoredProc(string name, params Tuple<string, string>[] parameters);
+        public abstract IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties);
+        public abstract IList<T> GetList(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties);
+        public abstract T GetSingle(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties);
+        public abstract void Remove(params T[] items);
+        public abstract void Update(params T[] items);
+    }
+}
