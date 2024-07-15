b8e0232cc5ffa1cdc97f13b71d085091f3ce4158
Brian Angyang <n01713422@humber.ca>
2024-07-15 16:04:49 -0400

    add constants class

---
+++ CareerCloud.ADODataAccessLayer/ApplicationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace CareerCloud.ADODataAccessLayer
+{
+    public sealed class ApplicationConstants
+    {
+        public const string CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb;Database=JOB_PORTAL_DB;Integrated Security=true";
+
+        private ApplicationConstants()
+        {
+
+        }
+    }
+}
