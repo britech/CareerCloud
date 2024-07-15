ef4ba7ac01779cc481b0fff403cc37e64d5ca112
Brian Angyang <n01713422@humber.ca>
2024-07-15 16:56:04 -0400

    implement bare bones

---
+++ CareerCloud.ADODataAccessLayer/ApplicantProfileRepository.cs
@@ -0,0 +1,44 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantProfileRepository : IDataRepository<ApplicantProfilePoco>
+    {
+        public void Add(params ApplicantProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantProfilePoco> GetAll(params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantProfilePoco> GetList(Expression<Func<ApplicantProfilePoco, bool>> where, params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantProfilePoco GetSingle(Expression<Func<ApplicantProfilePoco, bool>> where, params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/CompanyJobRepository.cs
@@ -0,0 +1,44 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class CompanyJobRepository : IDataRepository<CompanyJobPoco>
+    {
+        public void Add(params CompanyJobPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CompanyJobPoco> GetAll(params Expression<Func<CompanyJobPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CompanyJobPoco> GetList(Expression<Func<CompanyJobPoco, bool>> where, params Expression<Func<CompanyJobPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompanyJobPoco GetSingle(Expression<Func<CompanyJobPoco, bool>> where, params Expression<Func<CompanyJobPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params CompanyJobPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params CompanyJobPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
