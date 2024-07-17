24ca8c81f42b2289eb46ec78d125cebbd61c9b71
Brian Angyang <n01713422@humber.ca>
2024-07-17 03:40:24 -0400

    remove static modifier from class, add new interface for more flexibility

--- CareerCloud.ADODataAccessLayer/DbHelper.cs
+++ CareerCloud.ADODataAccessLayer/DbHelper.cs
@@ -4,8 +4,15 @@ using System.Transactions;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public static class DbHelper
+    public class DbHelper
     {
+        private readonly string _connectionString;
+
+        public DbHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
         public static void Write(string query, IList<IDictionary<string, object?>> queryParams)
         {
             using (TransactionScope scope = new TransactionScope())
@@ -42,6 +49,48 @@ namespace CareerCloud.ADODataAccessLayer
                     using (DbDataReader reader = cmd.ExecuteReader())
                     {
                         while(reader.Read())
+                        {
+                            items.Add(mapper.MapRow(reader));
+                        }
+                    }
+                }
+            }
+            return items;
+        }
+
+        public void Update<T>(string query, IDbCommandParameterSetter<T> setter, T[] items) where T : class
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                using (DbConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    foreach (var item in items)
+                    {
+                        using (DbCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = query;
+                            setter.SetValues(cmd, item);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    scope.Complete();
+                }
+            }
+        }
+
+        public IList<T> Query<T>(string query, IDbRowMapper<T> mapper) where T : class
+        {
+            IList<T> items = new List<T>();
+            using(DbConnection connection = new SqlConnection(_connectionString))
+            {
+                using (DbCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = query;
+                    connection.Open();
+                    using (DbDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
                         {
                             items.Add(mapper.MapRow(reader));
                         }
---
+++ CareerCloud.ADODataAccessLayer/IDbCommandParameterSetter.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public interface IDbCommandParameterSetter<T>
+    {
+        void SetValues(DbCommand cmd, T item);
+    }
+}
