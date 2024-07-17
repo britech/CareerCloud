8aea2bb69e88ac24a9600734202f01e474c2d272
Brian Angyang <n01713422@humber.ca>
2024-07-16 21:43:50 -0400

    add helper classes to centralize DB processing

---
+++ CareerCloud.ADODataAccessLayer/DbHelper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.Common;
+using System.Transactions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public static class DbHelper
+    {
+        public static void WriteToDB(string query, IList<IDictionary<string, object?>> queryParams)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                using (DbConnection connection = new SqlConnection(ApplicationConstants.CONNECTION_STRING))
+                {
+                    connection.Open();
+                    foreach(var item in queryParams)
+                    {
+                        using (DbCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = query;
+                            foreach (KeyValuePair<string,object?> queryParam in item) {
+                                cmd.Parameters.Add(new SqlParameter(parameterName: queryParam.Key, value: queryParam.Value));
+                            }
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    scope.Complete();
+                }
+            }
+        }
+
+        public static IList<T> LoadFromDB<T>(string query, IDbRowMapper<T> mapper) where T : class
+        {
+            IList<T> items = new List<T>();
+            using (DbConnection connection = new SqlConnection(ApplicationConstants.CONNECTION_STRING))
+            {
+                using (DbCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = query;
+                    connection.Open();
+
+                    using (DbDataReader reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            items.Add(mapper.MapRow(reader));
+                        }
+                    }
+                }
+            }
+            return items;
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/IDbRowMapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public interface IDbRowMapper<T>
+    {
+        T MapRow(DbDataReader reader);
+    }
+}
