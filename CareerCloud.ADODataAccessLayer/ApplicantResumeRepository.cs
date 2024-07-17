fd713ff9c8053f3717e8b463c7428c40f9848f7e
Brian Angyang <n01713422@humber.ca>
2024-07-17 00:35:10 -0400

    implement CRUD for applicant_resumes

--- CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
@@ -1,14 +1,25 @@
 ﻿using CareerCloud.DataAccessLayer;
 using CareerCloud.Pocos;
+using System.Data.Common;
 using System.Linq.Expressions;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantResumeRepository : IDataRepository<ApplicantResumePoco>
+    public class ApplicantResumeRepository : IDataRepository<ApplicantResumePoco>, IDataRowMapper<ApplicantResumePoco>
     {
         public void Add(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            DbHelper.Write("insert into applicant_resumes(id, applicant, resume, last_updated) values(@id, @applicant, @resume, @last_updated)", items.AsEnumerable().Select(item =>
+            {
+                IDictionary<string, object?> queryParams = new Dictionary<string, object?>()
+                {
+                    { "@id", item.Id },
+                    { "@applicant", item.Applicant },
+                    { "@resume", item.Resume },
+                    { "@last_updated", item.LastUpdated }
+                };
+                return queryParams;
+            }).ToList());
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -18,7 +29,7 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<ApplicantResumePoco> GetAll(params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            return DbHelper.Load("select id, applicant, resume, last_updated from applicant_resumes", this);
         }
 
         public IList<ApplicantResumePoco> GetList(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
@@ -28,17 +39,45 @@ namespace CareerCloud.ADODataAccessLayer
 
         public ApplicantResumePoco GetSingle(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            return GetAll().AsQueryable().Where(where).FirstOrDefault(null as ApplicantResumePoco)!;
+        }
+
+        public ApplicantResumePoco MapRow(DbDataReader reader)
+        {
+            return new ApplicantResumePoco()
+            {
+                Id = reader.GetGuid(0),
+                Applicant = reader.GetGuid(1),
+                Resume = reader.GetString(2),
+                LastUpdated = reader.GetValue(3) as DateTime?
+            };
         }
 
         public void Remove(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            DbHelper.Write("delete from applicant_resumes where id = @id", items.AsEnumerable().Select(item =>
+            {
+                IDictionary<string, object?> queryParams = new Dictionary<string, object?>()
+                {
+                    { "@id", item.Id }
+                };
+                return queryParams;
+            }).ToList());
         }
 
         public void Update(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            DbHelper.Write("update applicant_resumes set applicant = @applicant, resume = @resume, last_updated = @last_updated where id = @id", items.AsEnumerable().Select(item =>
+            {
+                IDictionary<string, object?> queryParams = new Dictionary<string, object?>()
+                {
+                    { "@id", item.Id },
+                    { "@applicant", item.Applicant },
+                    { "@resume", item.Resume },
+                    { "@last_updated", item.LastUpdated }
+                };
+                return queryParams;
+            }).ToList());
         }
     }
 }
