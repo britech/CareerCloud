1d7616a9e642efe5d9f222dd702a9af3613dc03b
Brian Angyang <n01713422@humber.ca>
2024-07-17 17:41:52 -0400

    remove unused methods

--- CareerCloud.ADODataAccessLayer/DbHelper.cs
+++ CareerCloud.ADODataAccessLayer/DbHelper.cs
@@ -13,51 +13,6 @@ namespace CareerCloud.ADODataAccessLayer
             _connectionString = connectionString;
         }
 
-        public static void Write(string query, IList<IDictionary<string, object?>> queryParams)
-        {
-            using (TransactionScope scope = new TransactionScope())
-            {
-                using (DbConnection connection = new SqlConnection(ApplicationConstants.CONNECTION_STRING))
-                {
-                    connection.Open();
-                    foreach(var item in queryParams)
-                    {
-                        using (DbCommand cmd = connection.CreateCommand())
-                        {
-                            cmd.CommandText = query;
-                            foreach (KeyValuePair<string,object?> queryParam in item) {
-                                cmd.Parameters.Add(new SqlParameter(parameterName: queryParam.Key, value: queryParam.Value));
-                            }
-                            cmd.ExecuteNonQuery();
-                        }
-                    }
-                    scope.Complete();
-                }
-            }
-        }
-
-        public static IList<T> Load<T>(string query, IDataRowMapper<T> mapper) where T : class
-        {
-            IList<T> items = new List<T>();
-            using (DbConnection connection = new SqlConnection(ApplicationConstants.CONNECTION_STRING))
-            {
-                using (DbCommand cmd = connection.CreateCommand())
-                {
-                    cmd.CommandText = query;
-                    connection.Open();
-
-                    using (DbDataReader reader = cmd.ExecuteReader())
-                    {
-                        while(reader.Read())
-                        {
-                            items.Add(mapper.MapRow(reader));
-                        }
-                    }
-                }
-            }
-            return items;
-        }
-
         public void Update<T>(string query, IDbCommandParameterSetter<T> setter, T[] items) where T : class
         {
             using (TransactionScope scope = new TransactionScope())
