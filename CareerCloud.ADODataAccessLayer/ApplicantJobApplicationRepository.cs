7d6d97008965ded8a42aee11707e4aaddd03d70a
Brian Angyang <n01713422@humber.ca>
2024-07-18 13:14:28 -0400

    replace with lambda functions

--- CareerCloud.ADODataAccessLayer/ApplicantJobApplicationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantJobApplicationRepository.cs
@@ -1,56 +1,81 @@
-﻿using CareerCloud.Pocos;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
 using Microsoft.Data.SqlClient;
-using System.Data.Common;
+using System.Linq.Expressions;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantJobApplicationRepository : BaseRepositoryImpl<ApplicantJobApplicationPoco>
+    public class ApplicantJobApplicationRepository : IDataRepository<ApplicantJobApplicationPoco>
     {
-        public ApplicantJobApplicationRepository() :
-            base(new DbHelper(ApplicationConstants.CONNECTION_STRING),
-                "insert into applicant_job_applications(id, applicant, job, application_date) values(@id, @applicant, @job, @application_date)",
-                "update applicant_job_applications set applicant = @applicant, job = @job, application_date = @application_date where id = @id",
-                "delete from applicant_job_applications where id = @id",
-                "select id, applicant, job, application_date, time_stamp from applicant_job_applications",
-                new UpdateCmdParameterSetterImpl(),
-                new DeleteCmdParameterSetterImpl(),
-                new RowMapperImpl())
-        {
+        private readonly DbHelper _dbHelper;
 
+        public ApplicantJobApplicationRepository()
+        {
+            _dbHelper = new DbHelper(ApplicationConstants.CONNECTION_STRING);
         }
 
-        private class UpdateCmdParameterSetterImpl : IDbCommandParameterSetter<ApplicantJobApplicationPoco>
+        public void Add(params ApplicantJobApplicationPoco[] items)
         {
-            public void SetValues(DbCommand cmd, ApplicantJobApplicationPoco item)
-            {
-                cmd.Parameters.Add(new SqlParameter(parameterName: "@id", value: item.Id));
-                cmd.Parameters.Add(new SqlParameter(parameterName: "@applicant", value: item.Applicant));
-                cmd.Parameters.Add(new SqlParameter(parameterName: "@job", value: item.Job));
-                cmd.Parameters.Add(new SqlParameter(parameterName: "@application_date", value: item.ApplicationDate));
-            }
-        }
-
-        private class DeleteCmdParameterSetterImpl : IDbCommandParameterSetter<ApplicantJobApplicationPoco>
-        {
-            public void SetValues(DbCommand cmd, ApplicantJobApplicationPoco item)
-            {
-                cmd.Parameters.Add(new SqlParameter(parameterName: "@id", value: item.Id));
-            }
-        }
-
-        private class RowMapperImpl : IDbRowMapper<ApplicantJobApplicationPoco>
-        {
-            public ApplicantJobApplicationPoco MapRow(DbDataReader reader)
-            {
-                return new ApplicantJobApplicationPoco()
+            _dbHelper.Update("insert into applicant_job_applications(id, applicant, job, application_date) values(@id, @applicant, @job, @application_date)",
+                (cmd, item) =>
                 {
-                    Id = reader.GetGuid(0),
-                    Applicant = reader.GetGuid(1),
-                    Job = reader.GetGuid(2),
-                    ApplicationDate = reader.GetDateTime(3),
-                    TimeStamp = reader.GetValue(4) as byte[] ?? []
-                };
-            }
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@id", value: item.Id));
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@applicant", value: item.Applicant));
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@job", value: item.Job));
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@application_date", value: item.ApplicationDate));
+                }, items);
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantJobApplicationPoco> GetAll(params Expression<Func<ApplicantJobApplicationPoco, object>>[] navigationProperties)
+        {
+            return _dbHelper.Query("select id, applicant, job, application_date, time_stamp from applicant_job_applications",
+                (reader) =>
+                {
+                    return new ApplicantJobApplicationPoco()
+                    {
+                        Id = reader.GetGuid(0),
+                        Applicant = reader.GetGuid(1),
+                        Job = reader.GetGuid(2),
+                        ApplicationDate = reader.GetDateTime(3),
+                        TimeStamp = reader.GetValue(4) as byte[] ?? []
+                    };
+                });
+        }
+
+        public IList<ApplicantJobApplicationPoco> GetList(Expression<Func<ApplicantJobApplicationPoco, bool>> where, params Expression<Func<ApplicantJobApplicationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantJobApplicationPoco GetSingle(Expression<Func<ApplicantJobApplicationPoco, bool>> where, params Expression<Func<ApplicantJobApplicationPoco, object>>[] navigationProperties)
+        {
+            return GetAll().AsQueryable().Where(where).FirstOrDefault(null as ApplicantJobApplicationPoco)!;
+        }
+
+        public void Remove(params ApplicantJobApplicationPoco[] items)
+        {
+            _dbHelper.Update("delete from applicant_job_applications where id = @id",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                }, items);
+        }
+
+        public void Update(params ApplicantJobApplicationPoco[] items)
+        {
+            _dbHelper.Update("update applicant_job_applications set applicant = @applicant, job = @job, application_date = @application_date where id = @id",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@id", value: item.Id));
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@applicant", value: item.Applicant));
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@job", value: item.Job));
+                    cmd.Parameters.Add(new SqlParameter(parameterName: "@application_date", value: item.ApplicationDate));
+                }, items);
         }
     }
 }
