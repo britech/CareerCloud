798bda21cf532aa7eabddf7feb0f30b765edcc0a
Brian Angyang <n01713422@humber.ca>
2024-07-18 17:03:26 -0400

    replace with lambda functions

--- CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
@@ -1,46 +1,39 @@
-﻿using CareerCloud.Pocos;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
 using Microsoft.Data.SqlClient;
-using System.Data.Common;
+using System.Linq.Expressions;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantResumeRepository : BaseRepositoryImpl<ApplicantResumePoco>
+    public class ApplicantResumeRepository : IDataRepository<ApplicantResumePoco>
     {
-        public ApplicantResumeRepository() :
-            base(new DbHelper(ApplicationConstants.CONNECTION_STRING),
-                "insert into applicant_resumes(id, applicant, resume, last_updated) values(@id, @applicant, @resume, @last_updated)",
-                "update applicant_resumes set applicant = @applicant, resume = @resume, last_updated = @last_updated where id = @id",
-                "delete from applicant_resumes where id = @id",
-                "select id, applicant, resume, last_updated from applicant_resumes",
-                new UpdateCmdParameterSetterImpl(),
-                new DeleteCmdParameterSetterImpl(),
-                new RowMapperImpl())
-        {
+        private readonly DbHelper _dbHelper;
 
+        public ApplicantResumeRepository()
+        {
+            _dbHelper = new DbHelper(ApplicationConstants.CONNECTION_STRING);
         }
 
-        private class UpdateCmdParameterSetterImpl : IDbCommandParameterSetter<ApplicantResumePoco>
+        public void Add(params ApplicantResumePoco[] items)
         {
-            public void SetValues(DbCommand cmd, ApplicantResumePoco item)
-            {
-                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
-                cmd.Parameters.Add(new SqlParameter("@applicant", item.Applicant));
-                cmd.Parameters.Add(new SqlParameter("@resume", item.Resume));
-                cmd.Parameters.Add(new SqlParameter("@last_updated", item.LastUpdated));
-            }
+            _dbHelper.Update("insert into applicant_resumes(id, applicant, resume, last_updated) values(@id, @applicant, @resume, @last_updated)",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                    cmd.Parameters.Add(new SqlParameter("@applicant", item.Applicant));
+                    cmd.Parameters.Add(new SqlParameter("@resume", item.Resume));
+                    cmd.Parameters.Add(new SqlParameter("@last_updated", item.LastUpdated));
+                }, items);
         }
 
-        private class DeleteCmdParameterSetterImpl : IDbCommandParameterSetter<ApplicantResumePoco>
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
         {
-            public void SetValues(DbCommand cmd, ApplicantResumePoco item)
-            {
-                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
-            }
+            throw new NotImplementedException();
         }
 
-        private class RowMapperImpl : IDbRowMapper<ApplicantResumePoco>
+        public IList<ApplicantResumePoco> GetAll(params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
         {
-            public ApplicantResumePoco MapRow(DbDataReader reader)
+            return _dbHelper.Query("select id, applicant, resume, last_updated from applicant_resumes", (reader) =>
             {
                 return new ApplicantResumePoco()
                 {
@@ -49,7 +42,38 @@ namespace CareerCloud.ADODataAccessLayer
                     Resume = reader.GetString(2),
                     LastUpdated = reader.GetValue(3) as DateTime?
                 };
-            }
+            });
+        }
+
+        public IList<ApplicantResumePoco> GetList(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantResumePoco GetSingle(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            return GetAll().AsQueryable().Where(where).FirstOrDefault(null as ApplicantResumePoco)!;
+        }
+
+        public void Remove(params ApplicantResumePoco[] items)
+        {
+            _dbHelper.Update("delete from applicant_resumes where id = @id",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                }, items);
+        }
+
+        public void Update(params ApplicantResumePoco[] items)
+        {
+            _dbHelper.Update("update applicant_resumes set applicant = @applicant, resume = @resume, last_updated = @last_updated where id = @id",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                    cmd.Parameters.Add(new SqlParameter("@applicant", item.Applicant));
+                    cmd.Parameters.Add(new SqlParameter("@resume", item.Resume));
+                    cmd.Parameters.Add(new SqlParameter("@last_updated", item.LastUpdated));
+                }, items);
         }
     }
 }
