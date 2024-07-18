7ceb3f3b5a74b26d0f2fe1c1577db5802fcd7db9
Brian Angyang <n01713422@humber.ca>
2024-07-18 18:16:45 -0400

    use lambda functions

--- CareerCloud.ADODataAccessLayer/ApplicantSkillRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantSkillRepository.cs
@@ -1,64 +1,93 @@
-﻿using CareerCloud.Pocos;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
 using Microsoft.Data.SqlClient;
-using System.Data.Common;
+using System.Linq.Expressions;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantSkillRepository : BaseRepositoryImpl<ApplicantSkillPoco>
+    public class ApplicantSkillRepository : IDataRepository<ApplicantSkillPoco>
     {
-        public ApplicantSkillRepository():
-            base(new DbHelper(ApplicationConstants.CONNECTION_STRING),
-                "insert into applicant_skills(id, applicant, skill, skill_level, start_month, start_year, end_month, end_year) values(@id, @applicant, @skill, @skill_level, @start_month, @start_year, @end_month, @end_year)", 
-                "update applicant_skills set applicant = @applicant, skill = @skill, skill_level = @skill_level, start_month = @start_month, start_year = @start_year, end_month = @end_month, end_year = @end_year where id = @id", 
-                "delete from applicant_skills where id = @id", 
-                "select id, applicant, skill, skill_level, start_month, start_year, end_month, end_year, time_stamp from applicant_skills", 
-                new UpdateCmdParameterSetterImpl(), 
-                new DeleteCmdParameterSetterImpl(), 
-                new RowMapperImpl())
-        {
+        private readonly DbHelper _dbHelper;
 
+        public ApplicantSkillRepository()
+        {
+            _dbHelper = new DbHelper(ApplicationConstants.CONNECTION_STRING);
         }
 
-        private class UpdateCmdParameterSetterImpl : IDbCommandParameterSetter<ApplicantSkillPoco>
+        public void Add(params ApplicantSkillPoco[] items)
         {
-            public void SetValues(DbCommand cmd, ApplicantSkillPoco item)
-            {
-                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
-                cmd.Parameters.Add(new SqlParameter("@applicant", item.Applicant));
-                cmd.Parameters.Add(new SqlParameter("@skill", item.Skill));
-                cmd.Parameters.Add(new SqlParameter("@skill_level", item.SkillLevel));
-                cmd.Parameters.Add(new SqlParameter("@start_month", item.StartMonth));
-                cmd.Parameters.Add(new SqlParameter("@start_year", item.StartYear));
-                cmd.Parameters.Add(new SqlParameter("@end_month", item.EndMonth));
-                cmd.Parameters.Add(new SqlParameter("@end_year", item.EndYear));
-            }
-        }
-
-        private class DeleteCmdParameterSetterImpl : IDbCommandParameterSetter<ApplicantSkillPoco>
-        {
-            public void SetValues(DbCommand cmd, ApplicantSkillPoco item)
-            {
-                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
-            }
-        }
-
-        private class RowMapperImpl : IDbRowMapper<ApplicantSkillPoco>
-        {
-            public ApplicantSkillPoco MapRow(DbDataReader reader)
-            {
-                return new ApplicantSkillPoco()
+            _dbHelper.Update("insert into applicant_skills(id, applicant, skill, skill_level, start_month, start_year, end_month, end_year) values(@id, @applicant, @skill, @skill_level, @start_month, @start_year, @end_month, @end_year)",
+                (cmd, item) =>
                 {
-                    Id = reader.GetGuid(0),
-                    Applicant = reader.GetGuid(1),
-                    Skill = reader.GetString(2),
-                    SkillLevel = reader.GetString(3),
-                    StartMonth = reader.GetByte(4),
-                    StartYear = reader.GetInt32(5),
-                    EndMonth = reader.GetByte(6),
-                    EndYear = reader.GetInt32(7),
-                    TimeStamp = reader.GetValue(8) as byte[] ?? []
-                };
-            }
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                    cmd.Parameters.Add(new SqlParameter("@applicant", item.Applicant));
+                    cmd.Parameters.Add(new SqlParameter("@skill", item.Skill));
+                    cmd.Parameters.Add(new SqlParameter("@skill_level", item.SkillLevel));
+                    cmd.Parameters.Add(new SqlParameter("@start_month", item.StartMonth));
+                    cmd.Parameters.Add(new SqlParameter("@start_year", item.StartYear));
+                    cmd.Parameters.Add(new SqlParameter("@end_month", item.EndMonth));
+                    cmd.Parameters.Add(new SqlParameter("@end_year", item.EndYear));
+                }, items);
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantSkillPoco> GetAll(params Expression<Func<ApplicantSkillPoco, object>>[] navigationProperties)
+        {
+            return _dbHelper.Query("select id, applicant, skill, skill_level, start_month, start_year, end_month, end_year, time_stamp from applicant_skills",
+                (reader) =>
+                {
+                    return new ApplicantSkillPoco()
+                    {
+                        Id = reader.GetGuid(0),
+                        Applicant = reader.GetGuid(1),
+                        Skill = reader.GetString(2),
+                        SkillLevel = reader.GetString(3),
+                        StartMonth = reader.GetByte(4),
+                        StartYear = reader.GetInt32(5),
+                        EndMonth = reader.GetByte(6),
+                        EndYear = reader.GetInt32(7),
+                        TimeStamp = reader.GetValue(8) as byte[] ?? []
+                    };
+                });
+        }
+
+        public IList<ApplicantSkillPoco> GetList(Expression<Func<ApplicantSkillPoco, bool>> where, params Expression<Func<ApplicantSkillPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantSkillPoco GetSingle(Expression<Func<ApplicantSkillPoco, bool>> where, params Expression<Func<ApplicantSkillPoco, object>>[] navigationProperties)
+        {
+            return GetAll().AsQueryable().Where(where).FirstOrDefault(null as ApplicantSkillPoco)!;
+        }
+
+        public void Remove(params ApplicantSkillPoco[] items)
+        {
+            _dbHelper.Update("delete from applicant_skills where id = @id",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                }, items);
+        }
+
+        public void Update(params ApplicantSkillPoco[] items)
+        {
+            _dbHelper.Update("update applicant_skills set applicant = @applicant, skill = @skill, skill_level = @skill_level, start_month = @start_month, start_year = @start_year, end_month = @end_month, end_year = @end_year where id = @id",
+                (cmd, item) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                    cmd.Parameters.Add(new SqlParameter("@applicant", item.Applicant));
+                    cmd.Parameters.Add(new SqlParameter("@skill", item.Skill));
+                    cmd.Parameters.Add(new SqlParameter("@skill_level", item.SkillLevel));
+                    cmd.Parameters.Add(new SqlParameter("@start_month", item.StartMonth));
+                    cmd.Parameters.Add(new SqlParameter("@start_year", item.StartYear));
+                    cmd.Parameters.Add(new SqlParameter("@end_month", item.EndMonth));
+                    cmd.Parameters.Add(new SqlParameter("@end_year", item.EndYear));
+                }, items);
         }
     }
 }
