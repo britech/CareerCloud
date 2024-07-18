c321a594f491997f3cfa9fea00b659ca627620b4
Brian Angyang <n01713422@humber.ca>
2024-07-17 21:28:45 -0400

    implement company_profiles

--- CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
@@ -1,44 +1,60 @@
-﻿using CareerCloud.DataAccessLayer;
-using CareerCloud.Pocos;
-using System.Linq.Expressions;
+﻿using CareerCloud.Pocos;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class CompanyProfileRepository : IDataRepository<CompanyProfilePoco>
+    public class CompanyProfileRepository : BaseRepositoryImpl<CompanyProfilePoco>
     {
-        public void Add(params CompanyProfilePoco[] items)
+        public CompanyProfileRepository() :
+            base(new DbHelper(ApplicationConstants.CONNECTION_STRING),
+                "insert into company_profiles(id, registration_date, company_website, contact_phone, contact_name, company_logo) values(@id, @registration_date, @company_website, @contact_phone, @contact_name, @company_logo)",
+                "update company_profiles set registration_date = @registration_date, company_website = @company_website, contact_phone = @contact_phone, contact_name = @contact_name, company_logo = @company_logo where id = @id",
+                "delete from company_profiles where id = @id",
+                "select id, registration_date, company_website, contact_phone, contact_name, company_logo, time_stamp from company_profiles",
+                new UpdateCmdParameterSetterImpl(),
+                new DeleteCmdParameterSetterImpl(),
+                new RowMapperImpl())
         {
-            throw new NotImplementedException();
+
         }
 
-        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        private class UpdateCmdParameterSetterImpl : IDbCommandParameterSetter<CompanyProfilePoco>
         {
-            throw new NotImplementedException();
+            public void SetValues(DbCommand cmd, CompanyProfilePoco item)
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                cmd.Parameters.Add(new SqlParameter("@registration_date", item.RegistrationDate));
+                cmd.Parameters.Add(new SqlParameter("@company_website", item.CompanyWebsite));
+                cmd.Parameters.Add(new SqlParameter("@contact_phone", item.ContactPhone));
+                cmd.Parameters.Add(new SqlParameter("@contact_name", item.ContactName));
+                cmd.Parameters.Add(new SqlParameter("@company_logo", item.CompanyLogo));
+            }
         }
 
-        public IList<CompanyProfilePoco> GetAll(params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        private class DeleteCmdParameterSetterImpl : IDbCommandParameterSetter<CompanyProfilePoco>
         {
-            throw new NotImplementedException();
+            public void SetValues(DbCommand cmd, CompanyProfilePoco item)
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+            }
         }
 
-        public IList<CompanyProfilePoco> GetList(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        private class RowMapperImpl : IDbRowMapper<CompanyProfilePoco>
         {
-            throw new NotImplementedException();
-        }
-
-        public CompanyProfilePoco GetSingle(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(params CompanyProfilePoco[] items)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(params CompanyProfilePoco[] items)
-        {
-            throw new NotImplementedException();
+            public CompanyProfilePoco MapRow(DbDataReader reader)
+            {
+                return new CompanyProfilePoco()
+                {
+                    Id = reader.GetGuid(0),
+                    RegistrationDate = reader.GetDateTime(1),
+                    CompanyWebsite = reader.GetValue(2) as string ?? null,
+                    ContactPhone = reader.GetString(3),
+                    ContactName = reader.GetValue(4) as string ?? null,
+                    CompanyLogo = reader.GetValue(5) as byte[] ?? null,
+                    TimeStamp = reader.GetValue(6) as byte[] ?? null
+                };
+            }
         }
     }
 }
