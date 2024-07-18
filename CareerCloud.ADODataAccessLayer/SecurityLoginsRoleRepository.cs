500d3d0541d3598b6fbdcd2dee1977cdb15c6c76
Brian Angyang <n01713422@humber.ca>
2024-07-18 00:58:05 -0400

    implement security_logins_roles

--- CareerCloud.ADODataAccessLayer/SecurityLoginsRoleRepository.cs
+++ CareerCloud.ADODataAccessLayer/SecurityLoginsRoleRepository.cs
@@ -1,44 +1,54 @@
-﻿using CareerCloud.DataAccessLayer;
-using CareerCloud.Pocos;
-using System.Linq.Expressions;
+﻿using CareerCloud.Pocos;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class SecurityLoginsRoleRepository : IDataRepository<SecurityLoginsRolePoco>
+    public class SecurityLoginsRoleRepository : BaseRepositoryImpl<SecurityLoginsRolePoco>
     {
-        public void Add(params SecurityLoginsRolePoco[] items)
+        public SecurityLoginsRoleRepository() :
+            base(new DbHelper(ApplicationConstants.CONNECTION_STRING),
+                "insert into security_logins_roles(id, login, role) values(@id, @login, @role)",
+                "update security_logins_roles set login = @login, role = @role where id = @id",
+                "delete from security_logins_roles where id = @id",
+                "select id, login, role, time_stamp from security_logins_roles",
+                new UpdateCmdParameterSetterImpl(),
+                new DeleteCmdParameterSetterImpl(),
+                new RowMapperImpl())
         {
-            throw new NotImplementedException();
+
         }
 
-        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        private class UpdateCmdParameterSetterImpl : IDbCommandParameterSetter<SecurityLoginsRolePoco>
         {
-            throw new NotImplementedException();
+            public void SetValues(DbCommand cmd, SecurityLoginsRolePoco item)
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                cmd.Parameters.Add(new SqlParameter("@login", item.Login));
+                cmd.Parameters.Add(new SqlParameter("@role", item.Role));
+            }
         }
 
-        public IList<SecurityLoginsRolePoco> GetAll(params Expression<Func<SecurityLoginsRolePoco, object>>[] navigationProperties)
+        private class DeleteCmdParameterSetterImpl : IDbCommandParameterSetter<SecurityLoginsRolePoco>
         {
-            throw new NotImplementedException();
+            public void SetValues(DbCommand cmd, SecurityLoginsRolePoco item)
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+            }
         }
 
-        public IList<SecurityLoginsRolePoco> GetList(Expression<Func<SecurityLoginsRolePoco, bool>> where, params Expression<Func<SecurityLoginsRolePoco, object>>[] navigationProperties)
+        private class RowMapperImpl : IDbRowMapper<SecurityLoginsRolePoco>
         {
-            throw new NotImplementedException();
-        }
-
-        public SecurityLoginsRolePoco GetSingle(Expression<Func<SecurityLoginsRolePoco, bool>> where, params Expression<Func<SecurityLoginsRolePoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(params SecurityLoginsRolePoco[] items)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(params SecurityLoginsRolePoco[] items)
-        {
-            throw new NotImplementedException();
+            public SecurityLoginsRolePoco MapRow(DbDataReader reader)
+            {
+                return new SecurityLoginsRolePoco()
+                {
+                    Id = reader.GetGuid(0),
+                    Login = reader.GetGuid(1),
+                    Role = reader.GetGuid(2),
+                    TimeStamp = reader.GetValue(3) as byte[] ?? null
+                };
+            }
         }
     }
 }
