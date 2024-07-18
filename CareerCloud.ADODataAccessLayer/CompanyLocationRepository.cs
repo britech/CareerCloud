a402ebc0d187ae5584157b0309c5d71584b6927b
Brian Angyang <n01713422@humber.ca>
2024-07-17 21:07:49 -0400

    implement company_locations

--- CareerCloud.ADODataAccessLayer/CompanyLocationRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyLocationRepository.cs
@@ -1,44 +1,62 @@
-﻿using CareerCloud.DataAccessLayer;
-using CareerCloud.Pocos;
-using System.Linq.Expressions;
+﻿using CareerCloud.Pocos;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class CompanyLocationRepository : IDataRepository<CompanyLocationPoco>
+    public class CompanyLocationRepository : BaseRepositoryImpl<CompanyLocationPoco>
     {
-        public void Add(params CompanyLocationPoco[] items)
+        public CompanyLocationRepository() :
+            base(new DbHelper(ApplicationConstants.CONNECTION_STRING),
+                "insert into company_locations(id, company, country_code, state_province_code, street_address, city_town, zip_postal_code) values(@id, @company, @country_code, @state_province_code, @street_address, @city_town, @zip_postal_code)",
+                "update company_locations set company = @company, country_code = @country_code, state_province_code = @state_province_code, street_address = @street_address, city_town = @city_town, zip_postal_code = @zip_postal_code where id = @id",
+                "delete from company_locations where id = @id",
+                "select id, company, country_code, state_province_code, street_address, city_town, zip_postal_code, time_stamp from company_locations",
+                new UpdateCmdParameterSetterImpl(),
+                new DeleteCmdParameterSetterImpl(),
+                new RowMapperImpl())
         {
-            throw new NotImplementedException();
+
         }
 
-        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        private class UpdateCmdParameterSetterImpl : IDbCommandParameterSetter<CompanyLocationPoco>
         {
-            throw new NotImplementedException();
+            public void SetValues(DbCommand cmd, CompanyLocationPoco item)
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+                cmd.Parameters.Add(new SqlParameter("@company", item.Company));
+                cmd.Parameters.Add(new SqlParameter("@country_code", item.CountryCode));
+                cmd.Parameters.Add(new SqlParameter("@state_province_code", item.Province));
+                cmd.Parameters.Add(new SqlParameter("@street_address", item.Street));
+                cmd.Parameters.Add(new SqlParameter("@city_town", item.City));
+                cmd.Parameters.Add(new SqlParameter("@zip_postal_code", item.PostalCode));
+            }
         }
 
-        public IList<CompanyLocationPoco> GetAll(params Expression<Func<CompanyLocationPoco, object>>[] navigationProperties)
+        private class DeleteCmdParameterSetterImpl : IDbCommandParameterSetter<CompanyLocationPoco>
         {
-            throw new NotImplementedException();
+            public void SetValues(DbCommand cmd, CompanyLocationPoco item)
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", item.Id));
+            }
         }
 
-        public IList<CompanyLocationPoco> GetList(Expression<Func<CompanyLocationPoco, bool>> where, params Expression<Func<CompanyLocationPoco, object>>[] navigationProperties)
+        private class RowMapperImpl : IDbRowMapper<CompanyLocationPoco>
         {
-            throw new NotImplementedException();
-        }
-
-        public CompanyLocationPoco GetSingle(Expression<Func<CompanyLocationPoco, bool>> where, params Expression<Func<CompanyLocationPoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(params CompanyLocationPoco[] items)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(params CompanyLocationPoco[] items)
-        {
-            throw new NotImplementedException();
+            public CompanyLocationPoco MapRow(DbDataReader reader)
+            {
+                return new CompanyLocationPoco()
+                {
+                    Id = reader.GetGuid(0),
+                    Company = reader.GetGuid(1),
+                    CountryCode = reader.GetString(2),
+                    Province = reader.GetValue(3) as string ?? null,
+                    Street = reader.GetValue(4) as string ?? null,
+                    City = reader.GetValue(5) as string ?? null,
+                    PostalCode = reader.GetValue(6) as string ?? null,
+                    TimeStamp = reader.GetValue(7) as byte[] ?? []
+                };
+            }
         }
     }
 }
