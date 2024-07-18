8a540a5c941ec5e75ebaa9f1b553d19691329020
Brian Angyang <n01713422@humber.ca>
2024-07-18 13:05:56 -0400

    add support for lambda function

--- CareerCloud.ADODataAccessLayer/DbHelper.cs
+++ CareerCloud.ADODataAccessLayer/DbHelper.cs
@@ -54,5 +54,49 @@ namespace CareerCloud.ADODataAccessLayer
             }
             return items;
         }
+
+        public void Update<T>(string query, Action<DbCommand, T> parameterSetterAction, T[] items) 
+            where T : class
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                using (DbConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    foreach (var item in items)
+                    {
+                        using (DbCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = query;
+                            parameterSetterAction(cmd, item);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    scope.Complete();
+                }
+            }
+        }
+
+        public IList<T> Query<T>(string query, Func<DbDataReader, T> rowMapperFunc) 
+            where T : class 
+        {
+            IList<T> items = new List<T>();
+            using (DbConnection connection = new SqlConnection(_connectionString))
+            {
+                using (DbCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = query;
+                    connection.Open();
+                    using (DbDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            items.Add(rowMapperFunc(reader));
+                        }
+                    }
+                }
+            }
+            return items;
+        }
     }
 }
