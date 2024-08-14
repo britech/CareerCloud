6cdcc70e1594b8e89b7a4fec082a19a89baacd62
Brian Angyang <n01713422@humber.ca>
2024-08-14 19:16:11 -0400

    should be no-op for CompanyJobPoco

--- CareerCloud.BusinessLogicLayer/CompanyJobLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyJobLogic.cs
@@ -5,20 +5,5 @@ namespace CareerCloud.BusinessLogicLayer;
 
 public class CompanyJobLogic(IDataRepository<CompanyJobPoco> repository) : BaseLogic<CompanyJobPoco>(repository)
 {
-    protected override void Verify(CompanyJobPoco[] pocos)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void Add(CompanyJobPoco[] pocos)
-    {
-        Verify(pocos);
-        base.Add(pocos);
-    }
-
-    public override void Update(CompanyJobPoco[] pocos)
-    {
-        Verify(pocos);
-        base.Update(pocos);
-    }
+    
 }
