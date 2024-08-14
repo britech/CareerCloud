afc477dcf4b789dfaad4bb197aa0cceb565df0c8
Brian Angyang <n01713422@humber.ca>
2024-08-14 19:13:47 -0400

    update validation logic of ApplicantProfilePoco

--- CareerCloud.BusinessLogicLayer/ApplicantProfileLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantProfileLogic.cs
@@ -7,7 +7,21 @@ public class ApplicantProfileLogic(IDataRepository<ApplicantProfilePoco> reposit
 {
     protected override void Verify(ApplicantProfilePoco[] pocos)
     {
-        throw new NotImplementedException();
+        PocoValidationHelper.Verify(poco =>
+        {
+            List<ValidationException> errors = [];
+            if (poco?.CurrentSalary < decimal.Zero)
+            {
+                errors.Add(ValidationException.CURRENT_SALARY_IS_NEGATIVE);
+            }
+
+            if (poco?.CurrentRate < decimal.Zero)
+            {
+                errors.Add(ValidationException.CURRENT_RATE_IS_NEGATIVE);
+            }
+
+            return errors;
+        }, pocos);
     }
 
     public override void Add(ApplicantProfilePoco[] pocos)
