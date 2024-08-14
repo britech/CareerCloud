c4a0455bf0cf785cff9534382d66223c37aa9ed9
Brian Angyang <n01713422@humber.ca>
2024-08-14 18:48:56 -0400

    update validation logic of ApplicantEducationPoco

--- CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -7,7 +7,26 @@ public class ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> rep
 {
     protected override void Verify(ApplicantEducationPoco[] pocos)
     {
-        throw new NotImplementedException();
+        PocoValidationHelper.Verify(poco =>
+        {
+            List<ValidationException> errors = [];
+            if (string.IsNullOrEmpty(poco?.Major) || poco?.Major?.Length < 3)
+            {
+                errors.Add(ValidationException.EDUCATION_MAJOR_REQUIRED);
+            }
+
+            if (poco?.StartDate > DateTime.Now)
+            {
+                errors.Add(ValidationException.EDUCATION_START_DATE_GREATER_THAN_TODAY);
+            }
+
+            if (poco?.CompletionDate < poco?.StartDate)
+            {
+                errors.Add(ValidationException.EDUCATION_COMPLETION_DATE_LESS_THAN_STARTDATE);
+            }
+
+            return errors;
+        }, pocos);
     }
 
     public override void Add(ApplicantEducationPoco[] pocos)
