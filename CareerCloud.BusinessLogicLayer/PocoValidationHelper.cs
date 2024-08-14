fd099e13afad31ec6e876fc6a33760270119dfd9
Brian Angyang <n01713422@humber.ca>
2024-08-14 11:19:54 -0400

    add helper class

---
+++ CareerCloud.BusinessLogicLayer/PocoValidationHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace CareerCloud.BusinessLogicLayer;
+
+public class PocoValidationHelper
+{
+    public static void Verify<TPoco>(Func<TPoco, List<ValidationException>> validationFunc, TPoco[] items) 
+    {
+        List<ValidationException> validationErrors = [];
+        foreach (var item in items) { 
+            List<ValidationException> errors = validationFunc(item);
+            if (errors?.Count > 0)
+            {
+                validationErrors.AddRange(errors);
+            }
+        }
+
+        if (validationErrors.Count > 0)
+        {
+            throw new AggregateException(validationErrors);
+        }
+    }
+}
