1e17495aea2b6df339e0e60fafe498ec7d503411
Brian Angyang <n01713422@humber.ca>
2024-08-17 11:04:36 -0400

    use PocoValidationRule instead of func delegate

--- CareerCloud.BusinessLogicLayer/PocoValidationHelper.cs
+++ CareerCloud.BusinessLogicLayer/PocoValidationHelper.cs
@@ -2,20 +2,25 @@
 
 public class PocoValidationHelper
 {
-    public static void Verify<TPoco>(Func<TPoco, List<ValidationException>> validationFunc, TPoco[] items) 
+    public static void Validate<TPoco>(PocoValidationRule<TPoco>[] rules, TPoco[] items)
     {
-        List<ValidationException> validationErrors = [];
-        foreach (var item in items) { 
-            List<ValidationException> errors = validationFunc(item);
-            if (errors?.Count > 0)
+        List<ValidationException> validationExceptions = [];
+
+        foreach (TPoco item in items)
+        {
+            foreach(PocoValidationRule<TPoco> rule in rules)
             {
-                validationErrors.AddRange(errors);
+                ValidationException result = rule.Validate(item);
+                if (result != null)
+                {
+                    validationExceptions.Add(result);
+                }
             }
         }
 
-        if (validationErrors.Count > 0)
+        if (validationExceptions.Count > 0)
         {
-            throw new AggregateException(validationErrors);
+            throw new AggregateException(validationExceptions);
         }
     }
 }
