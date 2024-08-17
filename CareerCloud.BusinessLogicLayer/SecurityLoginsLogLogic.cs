e119be146e07952314a3d1392baf7acaa20a2459
Brian Angyang <n01713422@humber.ca>
2024-08-17 11:03:39 -0400

    remove overrides

--- CareerCloud.BusinessLogicLayer/SecurityLoginsLogLogic.cs
+++ CareerCloud.BusinessLogicLayer/SecurityLoginsLogLogic.cs
@@ -5,20 +5,5 @@ namespace CareerCloud.BusinessLogicLayer;
 
 public class SecurityLoginsLogLogic(IDataRepository<SecurityLoginsLogPoco> repository) : BaseLogic<SecurityLoginsLogPoco>(repository)
 {
-    protected override void Verify(SecurityLoginsLogPoco[] pocos)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void Add(SecurityLoginsLogPoco[] pocos)
-    {
-        Verify(pocos);
-        base.Add(pocos);
-    }
-
-    public override void Update(SecurityLoginsLogPoco[] pocos)
-    {
-        Verify(pocos);
-        base.Update(pocos);
-    }
+   
 }
--- CareerCloud.BusinessLogicLayer/SecurityLoginsRoleLogic.cs
+++ CareerCloud.BusinessLogicLayer/SecurityLoginsRoleLogic.cs
@@ -5,20 +5,5 @@ namespace CareerCloud.BusinessLogicLayer;
 
 public class SecurityLoginsRoleLogic(IDataRepository<SecurityLoginsRolePoco> repository) : BaseLogic<SecurityLoginsRolePoco>(repository)
 {
-    protected override void Verify(SecurityLoginsRolePoco[] pocos)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void Add(SecurityLoginsRolePoco[] pocos)
-    {
-        Verify(pocos);
-        base.Add(pocos);
-    }
-
-    public override void Update(SecurityLoginsRolePoco[] pocos)
-    {
-        Verify(pocos);
-        base.Update(pocos);
-    }
+    
 }
