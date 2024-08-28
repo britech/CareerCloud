98dd9676ffd5b9426bc8ff1bbd6a9a6adea4a5b0
Brian Angyang <n01713422@humber.ca>
2024-08-28 01:42:35 -0400

    add configuration project

---
+++ CareerCloud.Configurations/CareerCloudConfigurationLoader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CareerCloud.Configurations
+{
+    public class CareerCloudConfigurationLoader
+    {
+        private static readonly Lazy<CareerCloudConfigurationLoader> _instance = new(() => new CareerCloudConfigurationLoader());
+        private readonly IConfiguration _configRoot;
+
+        public static CareerCloudConfigurationLoader Instance {  get { return _instance.Value; } }
+
+        private CareerCloudConfigurationLoader()
+        {
+            _configRoot = new ConfigurationBuilder()
+                .AddIniFile("CareerCloud.ini")
+                .Build();
+        }
+
+        public string GetConnectionString()
+        {
+            return _configRoot.GetConnectionString("JOB_PORTAL")!;
+        }
+    }
+}
