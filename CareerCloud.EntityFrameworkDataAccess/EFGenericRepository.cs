35016b123900716ba2d84d80070ee1bab6d6e079
Brian Angyang <n01713422@humber.ca>
2024-08-28 23:41:30 -0400

    initial commit of repository

---
+++ CareerCloud.EntityFrameworkDataAccess/EFGenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using CareerCloud.DataAccessLayer;
+using System.Linq.Expressions;
+
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    public class EFGenericRepository<T> : IDataRepository<T>
+        where T : class
+    {
+        public void Add(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetList(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
