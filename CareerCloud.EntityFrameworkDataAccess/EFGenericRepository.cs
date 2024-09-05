6abdaa468f86318bd30ffed6afeb19b3728d6ef7
Brian Angyang <n01713422@humber.ca>
2024-09-05 18:48:12 -0400

    delegate operation to registry

--- CareerCloud.EntityFrameworkDataAccess/EFGenericRepository.cs
+++ CareerCloud.EntityFrameworkDataAccess/EFGenericRepository.cs
@@ -6,9 +6,17 @@ namespace CareerCloud.EntityFrameworkDataAccess
     public class EFGenericRepository<T> : IDataRepository<T>
         where T : class
     {
+        private readonly EFRepositoryRegistry factory = EFRepositoryRegistry.Instance;
+        private readonly ICrudRepository<T> _repository;
+
+        public EFGenericRepository()
+        {
+            _repository = (factory.GetRepository(typeof(T)) as ICrudRepository<T>)!;   
+        }
+
         public void Add(params T[] items)
         {
-            throw new NotImplementedException();
+            _repository.AddAll(items);
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -28,17 +36,17 @@ namespace CareerCloud.EntityFrameworkDataAccess
 
         public T GetSingle(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            return _repository.FindOne(where);
         }
 
         public void Remove(params T[] items)
         {
-            throw new NotImplementedException();
+            _repository.RemoveAll(items);
         }
 
         public void Update(params T[] items)
         {
-            throw new NotImplementedException();
+            _repository.UpdateAll(items);
         }
     }
 }
