c7c052e0a0e548abb4de324d02769054e9c6d54e
Brian Angyang <n01713422@humber.ca>
2024-09-05 15:56:16 -0400

    add default factory implementation

---
+++ CareerCloud.DataAccessLayer/BaseRepositoryRegistry.cs
@@ -0,0 +1,27 @@
+﻿namespace CareerCloud.DataAccessLayer
+{
+    public class BaseRepositoryRegistry : IRepositoryFactory<Type>
+    {
+        private readonly IDictionary<Type, IRepository> _repositories;
+
+        public BaseRepositoryRegistry()
+        {
+            _repositories = new Dictionary<Type, IRepository>();
+        }
+
+        public BaseRepositoryRegistry(IDictionary<Type, IRepository> repositories)
+        {
+            _repositories = repositories;
+        }
+
+        public void RegisterRepository(Type type, IRepository repository)
+        {
+            _repositories.Add(type, repository);
+        }
+
+        public IRepository GetRepository(Type type)
+        {
+            return _repositories.TryGetValue(type, out IRepository? value) ? value : null!;
+        }
+    }
+}
