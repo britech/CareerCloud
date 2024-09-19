86da40530caea7fc97660ced7262d40a9f44b4e1
Brian Angyang <n01713422@humber.ca>
2024-09-19 19:15:24 -0400

    add type aware repository factory implementation

---
+++ CareerCloud.DataAccessLayer/TypeAwareRepositoryFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace CareerCloud.DataAccessLayer
+{
+    public class TypeAwareRepositoryFactory<T>(TypeAwareRepositoryRegistry registry) : IRepositoryFactory
+        where T : class
+    {
+        private readonly TypeAwareRepositoryRegistry _registry = registry;
+
+        public IRepository GetRepository()
+        {
+            return _registry.GetRepository(typeof(T));
+        }
+    }
+}
