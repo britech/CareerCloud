40563248bf65104ba1f54ad333fcb378151595a8
Brian Angyang <n01713422@humber.ca>
2024-09-19 19:14:31 -0400

    add registry interface

---
+++ CareerCloud.DataAccessLayer/IRepositoryRegistry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.DataAccessLayer
+{
+    public interface IRepositoryRegistry<T>
+    {
+        IRepository GetRepository(T id);
+    }
+}
