3d807b6bd213e0a3089913524c0470264e9e5cf0
Brian Angyang <brian.angyang@gmail.com>
2024-09-20 18:38:32 -0400

    add ini loader

---
+++ CareerCloud.Configurations/CareerCloudIniLoader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CareerCloud.Configurations
+{
+    public static class CareerCloudIniLoader
+    {
+        public static IConfiguration LoadConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddIniFile("CareerCloud.ini")
+                .Build();
+        }
+    }
+}
