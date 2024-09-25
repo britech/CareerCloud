50139585811bbd3640847cc23d8da8834d4dd12e
Brian Angyang <brian.angyang@gmail.com>
2024-09-24 21:24:35 -0400

    use json file, revert to singleton, set IConfiguration as property

---
+++ CareerCloud.Configurations/DefaultConfigurationLoader.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CareerCloud.Configurations;
+
+public class DefaultConfigurationLoader
+{
+    private static readonly Lazy<DefaultConfigurationLoader> _instance = new(() => new DefaultConfigurationLoader());
+
+    public static DefaultConfigurationLoader Instance { get { return _instance.Value; } }
+
+    public IConfiguration Configuration { get; init; }
+   
+    private DefaultConfigurationLoader()
+    {
+        Configuration = new ConfigurationBuilder().AddJsonFile("CareerCloud.json").Build();
+    }
+}
