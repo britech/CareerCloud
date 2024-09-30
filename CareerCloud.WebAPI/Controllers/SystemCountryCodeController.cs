dd5b62e69a0c1a552e1e7b44f9f783c33b2c4be7
Brian Angyang <brian.angyang@gmail.com>
2024-09-30 08:20:24 -0400

    integrate service to controller

--- CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
+++ CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
@@ -1,46 +1,50 @@
 ﻿using CareerCloud.BusinessLogicLayer;
 using CareerCloud.EntityFrameworkDataAccess;
 using CareerCloud.Pocos;
+using CareerCloud.WebAPI.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CareerCloud.WebAPI.Controllers
+namespace CareerCloud.WebAPI.Controllers;
+
+[Route("api/careercloud/v1/[controller]")]
+[ApiController]
+public class SystemCountryCodeController : ControllerBase
 {
-    [Route("api/careercloud/v1/[controller]")]
-    [ApiController]
-    public class SystemCountryCodeController : ControllerBase
+    private readonly AbstractValidatedPocoCRUDService<SystemCountryCodePoco, string> _service;
+
+    [ActivatorUtilitiesConstructor]
+    public SystemCountryCodeController(AbstractValidatedPocoCRUDService<SystemCountryCodePoco, string> service)
     {
-        private readonly AbstractValidatedPocoCRUDService<SystemCountryCodePoco, string> _service;
+        _service = service;
+    }
 
-        [ActivatorUtilitiesConstructor]
-        public SystemCountryCodeController(AbstractValidatedPocoCRUDService<SystemCountryCodePoco, string> service)
-        {
-            _service = service;
-        }
+    public SystemCountryCodeController()
+        : this(new SystemCountryCodeLogic(new EFGenericRepository<SystemCountryCodePoco>()))
+    {
 
-        public SystemCountryCodeController()
-            : this(new SystemCountryCodeLogic(new EFGenericRepository<SystemCountryCodePoco>()))
-        {
+    }
 
-        }
+    [HttpPost]
+    public ActionResult PostSystemCountryCode(SystemCountryCodePoco[] systemCountryCodePocos)
+    {
+        return ControllerHelper.ModifyItems<SystemCountryCodePoco[]>(_service.Add, systemCountryCodePocos);
+    }
 
-        public ActionResult PostSystemCountryCode(SystemCountryCodePoco[] systemCountryCodePocos)
-        {
-            throw new NotImplementedException();
-        }
+    [HttpGet]
+    public ActionResult GetSystemCountryCode(string code)
+    {
+        return ControllerHelper.GetItem<string, SystemCountryCodePoco>(_service.Get, code);
+    }
 
-        public ActionResult GetSystemCountryCode(string code)
-        {
-            throw new NotImplementedException();
-        }
+    [HttpPut]
+    public ActionResult PutSystemCountryCode(SystemCountryCodePoco[] systemCountryCodePocos)
+    {
+        return ControllerHelper.ModifyItems<SystemCountryCodePoco[]>(_service.Update, systemCountryCodePocos);
+    }
 
-        public ActionResult PutSystemCountryCode(SystemCountryCodePoco[] systemCountryCodePocos)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ActionResult DeleteSystemCountryCode(SystemCountryCodePoco[] systemCountryCodePocos)
-        {
-            throw new NotImplementedException();
-        }
+    [HttpDelete]
+    public ActionResult DeleteSystemCountryCode(SystemCountryCodePoco[] systemCountryCodePocos)
+    {
+        return ControllerHelper.ModifyItems<SystemCountryCodePoco[]>(_service.Delete, systemCountryCodePocos);
     }
 }
