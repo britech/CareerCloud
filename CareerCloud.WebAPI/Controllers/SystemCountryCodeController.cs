ea85d4d1f1f1cdd62b4e5cff248b03b0b02803b7
Brian Angyang <brian.angyang@gmail.com>
2024-10-02 00:27:32 -0400

    add documentation for system component

--- CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
+++ CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
@@ -1,8 +1,12 @@
 ﻿using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
+using CareerCloud.WebAPI.Constants;
 using CareerCloud.WebAPI.Core;
+using CareerCloud.WebAPI.Examples;
 using CareerCloud.WebAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
 
 namespace CareerCloud.WebAPI.Controllers;
 
@@ -20,28 +24,45 @@ public class SystemCountryCodeController : BaseCrudController<SystemCountryCodeP
 
     }
 
+    [SwaggerOperation(Summary = "Add countries", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "Countries added successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Input validation failed, check your entries.", typeof(ExampleValidationFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    [Consumes(MediaTypeNames.Application.Json)]
     [HttpPost]
-    public ActionResult PostSystemCountryCode(SystemCountryCodePoco[] items)
-    {
-        return Add(items);
-    }
+    public ActionResult PostSystemCountryCode(
+        [SwaggerRequestBody(Description = "The countries to add.", Required = true)] SystemCountryCodePoco[] items) => Add(items);
 
+    [SwaggerOperation(Summary = "Retrieve country info", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "The info", typeof(SystemCountryCodePoco), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Info is not available, ensure ID is correct.", typeof(ExampleNotFoundFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
     [HttpGet]
     [Route("{code}")]
-    public ActionResult GetSystemCountryCode(string code)
-    {
-        return FindById(code);
-    }
+    public ActionResult GetSystemCountryCode(
+        [SwaggerParameter(Description = "The country code", Required = true)] string code) => FindById(code);
 
+    [SwaggerOperation(Summary = "Update countries", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "Countries updated successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Input validation failed, check your entries.", typeof(ExampleValidationFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    [Consumes(MediaTypeNames.Application.Json)]
     [HttpPut]
-    public ActionResult PutSystemCountryCode(SystemCountryCodePoco[] items)
-    {
-        return Update(items);
-    }
+    public ActionResult PutSystemCountryCode(
+        [SwaggerRequestBody(Description = "The countries to update.", Required = true)] SystemCountryCodePoco[] items) => Update(items);
 
+    [SwaggerOperation(Summary = "Remove countries", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "Countries removed successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Input validation failed, check your entries.", typeof(ExampleValidationFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    [Consumes(MediaTypeNames.Application.Json)]
     [HttpDelete]
-    public ActionResult DeleteSystemCountryCode(SystemCountryCodePoco[] items)
-    {
-        return Delete(items);
-    }
+    public ActionResult DeleteSystemCountryCode(
+        [SwaggerRequestBody(Description = "The countries to remove.", Required = true)] SystemCountryCodePoco[] items) => Delete(items);
+
+    [SwaggerOperation(Summary = "List all countries", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "All countries", typeof(List<SystemCountryCodePoco>), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "Countries is empty. If expected not empty, try again later or call support.")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    public override ActionResult FindAll() => base.FindAll();
 }
--- CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
+++ CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
@@ -1,8 +1,12 @@
 ﻿using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
+using CareerCloud.WebAPI.Constants;
 using CareerCloud.WebAPI.Core;
+using CareerCloud.WebAPI.Examples;
 using CareerCloud.WebAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
 
 namespace CareerCloud.WebAPI.Controllers;
 
@@ -21,28 +25,44 @@ public class SystemLanguageCodeController : BaseCrudController<SystemLanguageCod
 
     }
 
+    [SwaggerOperation(Summary = "Add languages", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "Languages removed successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Input validation failed, check your entries.", typeof(ExampleValidationFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    [Consumes(MediaTypeNames.Application.Json)]
     [HttpDelete]
-    public ActionResult DeleteSystemLanguageCode([FromBody] SystemLanguageCodePoco[] items)
-    {
-        return Delete(items);
-    }
+    public ActionResult DeleteSystemLanguageCode(
+        [SwaggerRequestBody(Description = "The languages to remove", Required = true)] SystemLanguageCodePoco[] items) => Delete(items);
 
+    [SwaggerOperation(Summary = "Retrieve language info", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "The info", typeof(SystemLanguageCodePoco), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Info is not available, ensure ID is correct.", typeof(ExampleNotFoundFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
     [HttpGet]
     [Route("{id}")]
-    public ActionResult GetSystemLanguageCode([FromRoute] string id)
-    {
-        return FindById(id);
-    }
+    public ActionResult GetSystemLanguageCode([FromRoute] string id) => FindById(id);
 
+    [SwaggerOperation(Summary = "Add languages", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "Languages added successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Input validation failed, check your entries.", typeof(ExampleValidationFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    [Consumes(MediaTypeNames.Application.Json)]
     [HttpPost]
-    public ActionResult PostSystemLanguageCode([FromBody] SystemLanguageCodePoco[] items)
-    {
-        return Add(items);
-    }
+    public ActionResult PostSystemLanguageCode(
+        [SwaggerRequestBody(Description = "The languages to add", Required = true)] SystemLanguageCodePoco[] items) => Add(items);
 
+    [SwaggerOperation(Summary = "Update languages", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "Languages updated successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Input validation failed, check your entries.", typeof(ExampleValidationFault), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    [Consumes(MediaTypeNames.Application.Json)]
     [HttpPut]
-    public ActionResult PutSystemLanguageCode([FromBody] SystemLanguageCodePoco[] items)
-    {
-        return Update(items);
-    }
+    public ActionResult PutSystemLanguageCode(
+        [SwaggerRequestBody(Description = "The languages to update", Required = true)] SystemLanguageCodePoco[] items) => Update(items);
+
+    [SwaggerOperation(Summary = "List all languages", Tags = [TagNames.TAG_SYSTEM])]
+    [SwaggerResponse(StatusCodes.Status200OK, "All languages", typeof(List<SystemLanguageCodePoco>), [MediaTypeNames.Application.Json])]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "Languages is empty. If expected not empty, try again later or call support.")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "API Fault occured, try again later or call support.", typeof(ExampleGeneralFault), [MediaTypeNames.Application.Json])]
+    public override ActionResult FindAll() => base.FindAll();
 }
