e7432d129e5c7cc96323a23480173b03870c8695
Brian Angyang <brian.angyang@gmail.com>
2024-10-01 22:55:11 -0400

    add example definitions of faultdetail types

---
+++ CareerCloud.WebAPI/Examples/ExampleValidationFault.cs
@@ -0,0 +1,11 @@
+﻿using CareerCloud.WebAPI.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CareerCloud.WebAPI.Examples;
+
+[SwaggerSchemaFilter(typeof(ExampleValidationFaultSchemaFilter))]
+public class ExampleValidationFault : ProblemDetails
+{
+
+}
---
+++ CareerCloud.WebAPI/Filters/ExampleNotFoundFaultSchemaFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CareerCloud.WebAPI.Filters;
+
+public class ExampleNotFoundFaultSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        schema.Example = new OpenApiObject
+        {
+            ["type"] = new OpenApiString("https://tools.ietf.org/html/rfc9110#section-15.5.5"),
+            ["title"] = new OpenApiString("Not Found"),
+            ["status"] = new OpenApiInteger(StatusCodes.Status404NotFound),
+            ["traceId"] = new OpenApiString("00-77a728ff72dc2a92d6c395a05a9d7c50-247802088d3c6f0c-00")
+        };
+    }
+}
