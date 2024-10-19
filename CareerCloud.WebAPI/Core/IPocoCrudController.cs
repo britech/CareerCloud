4d8d2ba188227a009c580fdf516b0f0c183f97e1
Brian Angyang <brian.angyang@gmail.com>
2024-10-19 12:05:52 -0400

    set dependency in child class

--- CareerCloud.WebAPI/Core/IPocoCrudController.cs
+++ CareerCloud.WebAPI/Core/IPocoCrudController.cs
@@ -4,31 +4,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CareerCloud.WebAPI.Core;
 
-public abstract class IPocoCrudController<T>(BusinessLogicFactory factory) : AbstractCrudController<T, Guid>(factory)
+public abstract class IPocoCrudController<T>(BaseLogic<T> service) : AbstractCrudController<T, Guid>(service)
     where T : IPoco
 {
     protected override ActionResult Add(T[] items)
     {
-        return Save(Factory.CreateIPocoService<T>().Add, items);
+        return Save(service.Add, items);
     }
 
     protected override ActionResult Update(T[] items)
     {
-        return Save(Factory.CreateIPocoService<T>().Update, items);
+        return Save(service.Update, items);
     }
 
     protected override ActionResult Delete(T[] items)
     {
-        return Save(Factory.CreateIPocoService<T>().Delete, items);
+        return Save(service.Delete, items);
     }
 
     protected override ActionResult FindById(Guid id)
     {
-        return FindById(Factory.CreateIPocoService<T>().Get, id);
+        return FindById(service.Get, id);
     }
 
     public override ActionResult FindAll()
     {
-        return FindAll(() => Factory.CreateIPocoService<T>().GetAll()?.ToArray()!);
+        return FindAll(() => service.GetAll()?.ToArray()!);
     }
 }
