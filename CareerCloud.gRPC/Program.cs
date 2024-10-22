ea2e44f53c527bb1244e506aebcfff0d48be2db7
Brian Angyang <brian.angyang@gmail.com>
2024-10-22 01:53:27 -0400

    register new services

--- CareerCloud.gRPC/Program.cs
+++ CareerCloud.gRPC/Program.cs
@@ -13,12 +13,15 @@ builder.Services
     .AddPooledDbContextFactory<CareerCloudContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("JOB_PORTAL")))
     .AddSingleton<IDataRepositoryFactory, EFRepositoryFactory>()
     .AddSingleton(typeof(IDataRepository<>), typeof(EFGenericRepository<>))
-    .AddSingleton<BaseLogic<SecurityLoginPoco>, SecurityLoginLogic>();
+    .AddSingleton<BaseLogic<SecurityLoginPoco>, SecurityLoginLogic>()
+    .AddSingleton<BaseLogic<SecurityLoginsLogPoco>, SecurityLoginsLogLogic>()
+    .AddSingleton<AbstractValidatedPocoCRUDService<SystemLanguageCodePoco, string>, SystemLanguageCodeLogic>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<SecurityLoginService>();
+app.MapGrpcService<SecurityLoginsLogService>();
+app.MapGrpcService<SystemLanguageCodeService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
